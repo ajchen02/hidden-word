b5d59e329f34d900311cc9733526d4962d57db69
ajchen02 <88434425+ajchen02@users.noreply.github.com>
2025-08-21 18:30:30 +0800

    [update] 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-import { Context, Schema } from 'koishi'
+import { Context, Logger, Schema } from 'koishi'
+import { encode, decode } from './encoding'
+import { Session } from 'inspector/promises';
 
 export const name = 'hidden-word'
+export const reusable = false
+export const usage = `#
+hidden-word.encode "text" "secret" \n
+hidden-word.decode "text"
+`;
 
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+let log = new Logger('hidden-word')
+
+export function apply(ctx: Context, session: Session) {
+
+  // ctx.i18n.define('zh-CN', { noHidden: '该字符串无隐藏信息。' })
+  // ctx.i18n.define('en-US', { noHidden: 'This text has no hidden message.' })
+
+  ctx.command('hidden-word.encode <text> <secret>')
+  .action((_, text, secret) => encode(secret,text))
+
+  ctx.command('hidden-word.decode <text>')
+  .action((_, text) => {
+    let decoded = decode(text)
+    //log.info(decoded=='')
+    if (decoded=='') {return 'nop/无隐藏'}
+    else {return decoded;}
+  })
 }
