bb84f0295a07b1939c9346a612e76a368c124955
ajchen02 <88434425+ajchen02@users.noreply.github.com>
2025-08-21 22:48:06 +0800

    [update] 0.0.5

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ export const name = 'hidden-word'
 export const reusable = false
 export const usage = `#
 hidden-word.encode "text" "secret" \n
-hidden-word.decode "text"
+hidden-word.decode "text"\n\n
+
+hidden-word "text" ("secret")
 `;
 
 export interface Config {}
@@ -19,6 +21,12 @@ export function apply(ctx: Context, session: Session) {
 
   // ctx.i18n.define('zh-CN', { noHidden: '该字符串无隐藏信息。' })
   // ctx.i18n.define('en-US', { noHidden: 'This text has no hidden message.' })
+
+  ctx.command('hidden-word <text> [secret]')
+  .action((_,text,secret) => {
+    if (secret===undefined){return decode(text)}
+    else {return encode(secret,text)}
+  })
 
   ctx.command('hidden-word.encode <text> <secret>')
   .action((_, text, secret) => encode(secret,text))
